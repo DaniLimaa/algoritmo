9185399266e965fe3a599f8e0de6858c2d8c50a4
Daniel Camargo <169094692+DaniLimaa@users.noreply.github.com>
2024-05-23 11:57:13 -0300

    aula sobre operadores logicos

---
+++ aula02.js
@@ -0,0 +1,47 @@
+var numero1 = 10
+var numero2 = 14
+
+//assim que cria uma variavel dos valores da conta
+var soma = numero1 + numero2
+var subtracao = numero1 - numero2
+var multiplicacao = numero1 * numero2
+var divisao = numero1 / numero2
+
+console.log ("Soma dos valores", soma)
+console.log ("Subtração dos valores", subtracao)
+console.log ("Multiplicação dos valores", multiplicacao)
+console.log ("Divisão dos valores", divisao)
+
+//tarefa numero 1 anotar o que o paulin fala porque ela nao vai escreve na lousa
+//tarefa 2 imprimir uma tabuada 
+
+var numero_escolhido = 7
+
+var vezes1 = numero_escolhido * 1
+var vezes2 = numero_escolhido* 2
+var vezes3 = numero_escolhido * 3
+var vezes4 = numero_escolhido * 4
+var vezes5 = numero_escolhido * 5
+var vezes6 = numero_escolhido * 6
+var vezes7 = numero_escolhido * 7
+var vezes8 = numero_escolhido * 8
+var vezes9 = numero_escolhido * 9
+var vezes10 = numero_escolhido * 10
+
+console.log(numero_escolhido, "x 1 =",vezes1)
+console.log(numero_escolhido, "x 2 =",vezes2)
+console.log(numero_escolhido, "x 3 =",vezes3)
+console.log(numero_escolhido, "x 4 =",vezes4)
+console.log(numero_escolhido, "x 5 =",vezes5)
+console.log(numero_escolhido, "x 6 =",vezes6)
+console.log(numero_escolhido, "x 7 =",vezes7)
+console.log(numero_escolhido, "x 8 =",vezes8)
+console.log(numero_escolhido, "x 9 =",vezes9)
+console.log(numero_escolhido, "x 10 =",vezes10)
+
+console.log(!false)
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,43 @@
+// Aqui eu tenho  um comentario
+console.log("Olá Mundo")
+
+//variaveis sao coisas que mudam
+var nome = "Daniel"
+console.log("Imprimir nome: ")
+console.log(nome)
+
+//texto -> String (Guarda as informações)
+//numeros inteiros -> Int( Guarda apenas numeros inteiros, 1 2 3 )
+//numeros decimais -> Float / Double( Float tem uma capacidade menor : double tem uma capacidade maior de armazenamento de informações)
+//logicos -> Boolean( Sim : não, true : false, 0 : 1)
+
+var telefone = "19 98999-9999" //a representação de string são as aspas
+var ValorMonetario = 54.87 //seria o double ou float
+var ValorInteiro = 12 // seria o Int
+var VerdadeiroFalse = false //seria o Boolean
+
+// Guarde os dados do seu endereço em VARIAVEIS (ex. rua, bairro, cep, estado...)
+// Guarde os seus dados (nome, email, telefone, data de nascimento, genero) em VARIAVEIS
+//Crie uma VARIAVEL que diz que se voce é maior de idade
+//Crie uma VARIAVEL que diz se você pode dirigir
+
+var rua = "Serra da Saudade," //string
+var bairro = "Parque da liberdade,"
+var cep = "13470-412,"
+var estado = "São Paulo."
+console.log (rua, bairro, cep, estado)
+
+var nome = "Daniel Camargo de Lima,"
+var email = "daniel.camargodelima2@gmail.com,"
+var telefone = "19 96810-3559,"
+var dataDeNascimento = "10/01/2008,"
+var genero = "masculino."
+console.log (nome, email, telefone, dataDeNascimento, genero)
+
+var maior_de_idade = false
+console.log (maior_de_idade)
+
+var pode_dirigir = false
+console.log (pode_dirigir)
+
+
